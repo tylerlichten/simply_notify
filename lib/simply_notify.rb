4d72e1f075f2bf30ce7cdfdaba4dfbf8e3fc7344
Tyler Lichten <tlich10@gmail.com>
2016-02-29 14:53:18 -0500

    updated new_notification method, deleted undefined .email method

--- lib/simply_notify.rb
+++ lib/simply_notify.rb
@@ -2,12 +2,12 @@ require "simply_notify/version"
 require "action_mailer"
 
 class Notifier < ActionMailer::Base
-  default :from => 'no-reply@brandeis.edu'
+  default from: 'no-reply@brandeis.edu'
 
   def new_notification(recipient)
     @recipient = recipient
-    mail(:to => recipient.email, 
-      	 :subject => 'New Notification',
-         :content_type => 'text/html')
+    mail(to: @recipient, 
+      	 subject: 'New Notification',
+         content_type: 'text/html')
   end
 end
--- lib/simply_notify/version.rb
+++ lib/simply_notify/version.rb
@@ -1,3 +1,3 @@
 module SimplyNotify
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
 end
